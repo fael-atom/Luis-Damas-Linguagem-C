e9851e16efd1abe435cc390806d8e3b7142d76a8
Rafael Augusto Mota <rafael.augusto.mota@usp.br>
2022-09-01 15:32:13 -0300

    Adiciona programas do primeiro capítulo

---
+++ capitulo-01/prog0104.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main(){
+    printf("C\n");
+    printf("is the greatest\n");
+    printf("Language\n");
+
+    /* Pode utilizar uma única linha também */
+    printf("C\nis the greatest\nLanguage");
+}
