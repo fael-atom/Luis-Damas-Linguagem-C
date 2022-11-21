d12f9ca67039e998f1d9ee5f3e8db36c2d5028e1
Rafael Mota <fael.folow@gmail.com>
2022-11-21 12:16:38 -0300

    Adiciona novos exercíciosao capítulo 4

---
+++ exemplos/capitulo-04/prog0408.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main(){
+  int i,j;
+
+  for (i=1; i<=5; i=i+1){
+    for (j=1; j<=10; j=j+1){
+      printf("%2d * %2d = %2d\n",i,j,i*j);
+    }
+    /* Para não para na última linha */
+    if (i != 5){
+      printf("Pressione <ENTER> para continuar...");
+      getchar();      
+    }
+  }
+}
